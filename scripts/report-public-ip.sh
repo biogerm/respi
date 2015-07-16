57a481d0bd97aa02057938a95a9d11ca29c4924b
biogerm <biogerm@gmail.com>
2015-07-16 19:06:53 +0200

    Update report-ip

--- scripts/report-public-ip.sh
+++ scripts/report-public-ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-IP="$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')"
-#echo "${IP}"
 while true ; do
-ssh -i /home/pi/git/respi/scripts/ubuntuFree.pem ubuntu@50.18.192.139 bash -c "'echo `date +%Y%m%d' '%H:%M:%S` $IP >> /home/ubuntu/homeIp'"
-#echo "sent"
+IP="$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')"
+echo "${IP}"
+ssh -o ConnectTimeout=10 -i /home/pi/git/respi/scripts/ubuntuFree.pem ubuntu@50.18.192.139 bash -c "'echo `date +%Y%m%d' '%H:%M:%S` $IP >> /home/ubuntu/homeIp'"
+echo "sent"
 sleep 600
 done
