b4f7322c46df7e99a9e13802c6b0a8aad5fafb32
biogerm <biogerm@gmail.com>
2015-07-16 19:52:17 +0200

    Add no-ip.org IP update

--- scripts/report-public-ip.sh
+++ scripts/report-public-ip.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 while true ; do
+echo "Fetching IP"
 IP="$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')"
 echo "${IP}"
 ssh -o ConnectTimeout=10 -i /home/pi/git/respi/scripts/ubuntuFree.pem ubuntu@50.18.192.139 bash -c "'echo `date +%Y%m%d' '%H:%M:%S` $IP >> /home/ubuntu/homeIp'"
-echo "sent"
+echo "IP address is recored on AWS"
+echo "Updating no-ip.org"
+curl -X GET "http://biogerm:{PASSWORD}@dynupdate.no-ip.com/nic/update?hostname=biogerm.no-ip.org&myip="${IP}
 sleep 600
 done
