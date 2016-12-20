56e7fe00b195bd9d8a8a88631331bd747efa65e0
biogerm <biogerm@gmail.com>
2016-12-20 19:46:12 +0100

    updated DDNS address and certificate

--- scripts/report-public-ip.sh
+++ scripts/report-public-ip.sh
@@ -3,9 +3,9 @@ while true ; do
 echo "Fetching IP"
 IP="$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')"
 echo "${IP}"
-ssh -o ConnectTimeout=10 -i /home/pi/git/respi/scripts/ubuntuFree.pem ubuntu@50.18.192.139 bash -c "'echo `date +%Y%m%d' '%H:%M:%S` $IP >> /home/ubuntu/homeIp'"
+ssh -o ConnectTimeout=10 -i /home/pi/git/respi/scripts/trainingpair.pem ubuntu@biogerm.no-ip.org bash -c "'echo `date +%Y%m%d' '%H:%M:%S` $IP >> /home/ubuntu/homeIp'"
 echo "IP address is recored on AWS"
 echo "Updating no-ip.org"
-curl -X GET "http://biogerm:{PASSWORD}@dynupdate.no-ip.com/nic/update?hostname=biogerm.no-ip.org&myip="${IP}
+curl -X GET "http://biogerm:{PASSWORD}@dynupdate.no-ip.com/nic/update?hostname=biogerm.noip.me&myip="${IP}
 sleep 600
 done
