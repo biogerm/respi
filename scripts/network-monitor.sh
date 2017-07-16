5ea5cc2a7820219f155cd435ff65b9682b12a2a1
BiOgErM <biogerm@github.com>
2017-07-16 21:42:38 +0000

    Adding timestamp for network monitor

--- scripts/network-monitor.sh
+++ scripts/network-monitor.sh
@@ -4,7 +4,7 @@ while true ; do
    if ifconfig wlan0 | grep -q "inet addr:" ; then
       sleep 60
    else
-      echo "Network connection down! Attempting reconnection."
+      echo `date +%Y%m%d' '%H:%M:%S`" Network connection down! Attempting reconnection."
       ifup --force wlan0
       sleep 10
    fi
