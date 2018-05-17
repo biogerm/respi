deacd8fc9e1adce225a58f5d0eeb4b5a5117b152
BiOgErM <biogerm@github.com>
2018-05-17 15:41:16 +0000

    Update network monitor search keyword

--- scripts/network-monitor.sh
+++ scripts/network-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true ; do
-   if ifconfig wlan0 | grep -q "inet addr:" ; then
+   if ifconfig wlan0 | grep -q "inet" ; then
       sleep 60
    else
       echo `date +%Y%m%d' '%H:%M:%S`" Network connection down! Attempting reconnection."
