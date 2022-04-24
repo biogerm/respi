0b8aed36455d2c1ee52cbcaf89b7fc5cc18b80eb
BiOgErM <biogerm@github.com>
2022-04-24 18:13:40 +0200

    Add script to secure enable/disable vpn connection

---
+++ scripts/enablevpncron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ~/.profile
+LAST_LINE=$(tail -n 1 /home/pi/cronlog/`date "+%Y%m%d"`.log)
+#echo $LAST_LINE
+if [[ $LAST_LINE == *"Success"* ]]; then
+    echo "Already enabled, skip."
+else
+    /home/pi/git/respi/scripts/milogin.py 192.168.0.1 $ROUTER_PASSWORD enable >> /home/pi/cronlog/`date "+%Y%m%d"`.log 2>&1
+fi
+
